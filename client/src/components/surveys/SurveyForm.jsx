a911bfa902622c1e43a9085fc24f54004e40927f
David Pineda <ahbridge@gmail.com>
2018-08-06 13:28:15 +0800

    New Survey and Styling the Form

---
+++ client/src/components/surveys/SurveyForm.jsx
@@ -0,0 +1,41 @@
+// SURVEY FORM contains a basic form
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
+import SurveyField from './SurveyField'
+import { Link } from 'react-router-dom'
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title'},
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body'},
+    { label: 'Recipient List', name: 'emails'}
+]
+
+class SurveyForm extends Component {
+    renderFields() {
+
+        // ({ label, name }) are destructures from field.label and field.name
+        return _.map(FIELDS, ({ label, name }) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">NEXT<i className="material-icons right">done</i></button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
+import { reducer as reduxForm } from 'redux-form'
 import authReducer from './authReducer'
 
+
+// combineReducers takes all reducers and assigns them all to state properties
+
+
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: reduxForm  
 })
