012271734b1d1f3cc6a0b297830afa6fefa59c24
David Pineda <ahbridge@gmail.com>
2018-08-07 17:56:39 +0800

    create get surveys route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,7 +12,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id }) // show all records for the current user id
+    
+        res.send(surveys)
+    })
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('thanks for submitting your vote!')
     })
 
